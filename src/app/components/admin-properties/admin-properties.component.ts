1d51d83c463434c640eec6d94babc0616c8e9132
Delache <delache33@gmail.com>
2019-12-19 21:05:55 +0100

    feat: reactiveFormValidators

--- src/app/components/admin-properties/admin-properties.component.ts
+++ src/app/components/admin-properties/admin-properties.component.ts
@@ -1,4 +1,6 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-admin-properties',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-properties.component.css']
 })
 export class AdminPropertiesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  propertiesForm: FormGroup;
+  constructor(private formbuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.initPropertiesForm();
+  }
+  initPropertiesForm() {
+    this.propertiesForm = this.formbuilder.group({
+      title: ['', Validators.required],
+      category: ['', Validators.required],
+      surface: ['', Validators.required],
+      rooms: ['', Validators.required],
+      description: '',
+      price: ['', Validators.required]
+    });
+  }
+  onSubmitPropertiesForm() {
+    console.log(this.propertiesForm.value);
+  }
 }
